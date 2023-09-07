1550b0af503ee9e10188400fe1c00d58de206993
AlexYeshotkin <eshtoki@gmail.com>
2023-09-07 12:52:45 +0300

    feat: add splash screen and icons

--- App.tsx
+++ App.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import {StyleSheet} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import RootNavigation from './src/navigation/RootNavigator'
+import SplashScreen from 'react-native-splash-screen'
 
 function App(): JSX.Element {
+  React.useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <NavigationContainer>
       <RootNavigation />
